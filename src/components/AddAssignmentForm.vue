69697e0a3bf1a1f156b480f021101b3814c10aca
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-02-11 09:01:04 -0600

    Add form component template

--- src/components/AddAssignmentForm.vue
+++ src/components/AddAssignmentForm.vue
@@ -46,6 +46,35 @@ function addAssignment() {
 </script>
 
 <template>
+    
+    <button v-if="formHidden" @click="toggleForm" class="add-assignment"> + Add Assignment<button>
+
+    <section class="assignment-form">
+        <form v-if="!formHidden" @submit.prevent="addAssignment">
+        <section class="course-input">
+            <label for="course">Course:</label>
+            <input type="text" v-model="courseName" name="course" id="course" placeholder="Course name..." maxlength="20" required>
+        </section>
+
+        <section class="assignment-input">
+            <label for="assignment">Assignment:</label>
+            <input type="text" v-model="assignmentName" name="Assignment" id="assignment" placeholder="New assignment..." required>
+        </section>
+            
+        <section class="due-date-input">
+            <label for="due-date">Due Date:</label>
+            <input type="date" v-model="dueDate" name="due-date" id="due-date" :min="minDate" required>
+        </section>
+            
+        <section class="controls">
+            <button type="submit">Submit</button>
+            <button @click="toggleForm">Cancel</button>
+        </section>
+            
+        </form>
+        
+    </section>
+   
 </template>
 
 <style scoped>
