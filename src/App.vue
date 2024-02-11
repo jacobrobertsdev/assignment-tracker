8e6d17b32c7bde3d990c28969a8bead1fa8c152e
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-02-11 08:44:39 -0600

    Add app component script

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,34 @@
 <script setup>
+import AddAssignmentForm from './components/AddAssignmentForm.vue'
+import AssignmentList from './components/AssignmentList.vue'
+import { ref, onMounted } from 'vue'
+
+
+const assignmentsList = ref([])
+
+onMounted(() => {
+    const savedAssignments = localStorage.getItem('assignments') || []
+    if (savedAssignments) assignmentsList.value = JSON.parse(savedAssignments)
+})
+
+function setLocalStorage(list) {
+    localStorage.setItem('assignments', JSON.stringify(list))
+}
+
+function handleAddAssignment(newAssignment) {
+    assignmentsList.value.push(newAssignment)
+    setLocalStorage(assignmentsList.value)
+}
+
+function handleDeleteAssignment(id) {
+    assignmentsList.value = assignmentsList.value.filter(assignment => assignment.id !== id)
+    setLocalStorage(assignmentsList.value)
+}
+
 </script>
 
 <template>
+
 </template>
 
 <style scoped>
