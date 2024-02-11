ea0cb99d4e96fd2b56b14896bfa683f3765f5191
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-02-11 09:05:20 -0600

    Add script and template for list item component

--- src/components/AssignmentItem.vue
+++ src/components/AssignmentItem.vue
@@ -1,7 +1,26 @@
 <script setup>
+defineProps({
+    course: String,
+    name: String,
+    due: String,
+    id: String
+});
+
+const emit = defineEmits(['deleteAssignment'])
+
+function deleteAssignment(id) {
+    emit('deleteAssignment', id)
+}
+
 </script>
 
 <template>
+<div class="assignment">
+    <h3>{{ name }}</h3>
+    <span class="course-name">{{ course }}</span>
+    <span class="due-date">Due: {{ due }}</span>
+    <button class="delete" @click="deleteAssignment(id)">Delete</button>
+</div>
 </template>
 
 <style scoped>
