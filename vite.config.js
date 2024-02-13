069a3be313434ab552b75c4520f47ac899371a05
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-02-13 16:03:42 -0600

    Run build. Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "/assignment-tracker/",
-  resolve: {
-    alias: [],
-  },
 });
