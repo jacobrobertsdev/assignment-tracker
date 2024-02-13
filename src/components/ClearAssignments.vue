44334c7d044bf8569294a1207b03eaa21101a377
Jacob Roberts <jacobroberts.dev@gmail.com>
2024-02-13 09:36:50 -0600

    Install sweetAlert2. Add confirm window

--- src/components/ClearAssignments.vue
+++ src/components/ClearAssignments.vue
@@ -1,13 +1,28 @@
 <script setup>
 import { defineEmits } from 'vue';
+import Swal from 'sweetalert2'
 
 const emit = defineEmits(['clearAll'])
 
 function handleClear() {
-  const confirmed = window.confirm("Clear all of your assignments?")
-  if (confirmed) {
-    emit('clearAll')
+  // const confirmed = window.confirm("Clear all of your assignments?")
+  // if (confirmed) {
+  //   emit('clearAll')
+  // }
+
+  Swal.fire({
+  title: "Clear all assignments?",
+  text: "This will permanently delete all of your current assignments",
+  icon: "warning",
+  showCancelButton: true,
+  confirmButtonColor: "#1d56c9",
+  cancelButtonColor: "#d33",
+  confirmButtonText: "Confirm"
+}).then((result) => {
+  if (result.isConfirmed) {
+  emit('clearAll')
   }
+});
 }
 </script>
 
@@ -25,7 +40,7 @@ button {
     color: white;
     padding:.5rem .6rem .4rem .6rem;
     border: none;
-    background-color: hsl(220, 75%, 45%);
+    background-color: #1d56c9;
     border-radius:5px
 }
 
